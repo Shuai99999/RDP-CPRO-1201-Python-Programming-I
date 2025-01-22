24de4d48347b6495fd2a06b0f8ef9fb05fbc79b5
Shuai99999 <gaoshuai099@gmail.com>
2025-01-22 11:44:50 -0700

    l

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
-print('HelloWorld')
+last_name = input('What is your last name?')
+
+name_length = len(last_name)
+
+print(f'Your name has {name_length} letters')
+
+last_name = last_name.lower()
+
+print('z' in last_name)
